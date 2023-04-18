aebb172ce54bef1f44b6ff0a96d2cac29c46d646
costin1989 <costin0227@gmail.com>
2023-04-18 15:38:20 +0300

    validate requests

--- accounts.php
+++ accounts.php
@@ -5,7 +5,7 @@ include('api_key.php');
 // Set the content type to JSON
 header('Content-Type: application/json');
 
-// Create a new order
+// Create a new account
 function createAccount($data) {
     global $conn;
 
@@ -13,9 +13,12 @@ function createAccount($data) {
     $api_key = generate_api_key();
     $username = mysqli_real_escape_string($conn,  $data['username']);
     $expires_on = mysqli_real_escape_string($conn, $data['expires_on']);
+    $max_orders = mysqli_real_escape_string($conn, $data['max_orders']);
+    $status = 'to_be_verified';
 
     // Prepare the SQL query
-    $sql = "INSERT INTO accounts (api_key, username, expires_on) VALUES ('$api_key', '$username', '$expires_on')";
+    $sql = "INSERT INTO accounts (api_key, username, expires_on, max_orders, status) 
+            VALUES ('$api_key', '$username', '$expires_on', '$max_orders', '$status')";
 
     // Execute the query
     if (mysqli_query($conn, $sql)) {
@@ -25,7 +28,7 @@ function createAccount($data) {
     }
 }
 
-// Retrieve all orders
+// Retrieve all accounts
 function readAccounts() {
     global $conn;
 
@@ -49,7 +52,7 @@ function readAccounts() {
     return $accounts;
 }
 
-// Retrieve order by id
+// Retrieve account by id
 function readAccount($id) {
     global $conn;
 
@@ -64,24 +67,30 @@ function readAccount($id) {
         return array('error' => 'Error retrieving accounts: ' . mysqli_error($conn));
     }
 
+    if (mysqli_num_rows($result) != 1) {
+        return array('error' => 'Error retrieving account with id: '. $id);
+    }
+
     // Build the response array
     $accounts = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $accounts[] = $row;
     }
 
-    return $accounts;
+    return $accounts[0];
 }
 
-function validate_account($api_key){
+// Retrieve account by api key
+function readAccountByKey($api_key){
     global $conn;
 
     // Prepare the SQL query
-    $query = "SELECT id FROM accounts WHERE api_key = '$api_key'";
+    $query = "SELECT a.id, a.max_orders, a.status, r.name as role_name FROM accounts a
+                LEFT JOIN accounts_roles ar ON ar.account_id = a.id
+                LEFT JOIN roles r ON ar.role_id = r.id WHERE api_key = '$api_key'";
     $result = mysqli_query($conn, $query);
     if (mysqli_num_rows($result) != 1) {
-        header('HTTP/1.1 401 Unauthorized');
-        exit();
+        return array('error' => 'Error retrieving account with api key: '. $api_key);
     } else {
         // Build the response array
         $accounts = array();
@@ -92,7 +101,7 @@ function validate_account($api_key){
     }
 }
 
-// Update an existing order
+// Update an existing account
 function updateAccount($id, $data) {
     global $conn;
 
@@ -100,9 +109,12 @@ function updateAccount($id, $data) {
     $api_key = mysqli_real_escape_string($conn, $data['api_key']);
     $username = mysqli_real_escape_string($conn, $data['username']);
     $expires_on = mysqli_real_escape_string($conn, $data['expires_on']);
+    $max_orders = mysqli_real_escape_string($conn, $data['max_orders']);
+    $status = mysqli_real_escape_string($conn, $data['status']);
 
     // Prepare the SQL query
-    $sql = "UPDATE accounts SET api_key='$api_key', username='$username', expires_on='$expires_on' WHERE id=$id";
+    $sql = "UPDATE accounts SET api_key='$api_key', username='$username', expires_on='$expires_on', max_orders='$max_orders', status='$status' 
+                WHERE id=$id";
 
     // Execute the query
     if (mysqli_query($conn, $sql)) {
@@ -112,7 +124,7 @@ function updateAccount($id, $data) {
     }
 }
 
-// Delete an existing order
+// Delete an existing account
 function deleteAccount($id) {
     global $conn;
 
@@ -132,34 +144,55 @@ $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 if(in_array('accounts.php', $uri)){
+    //API key validation
+    $apiKey = isset($_GET['api_key']) ? $_GET['api_key'] : '';
+    $account = readAccountByKey($apiKey);
     switch ($method) {
         case 'GET':
             $id = $_GET['id'] ?? null;
             if ($id) {
                 $id = mysqli_real_escape_string($conn, $id);
-                $response = readAccount($id);
-            } else {
+                if($account['role_name'] == 'admin' || $account['id'] == $id){
+                    $response = readAccount($id);
+                    echo json_encode($response);
+                    break;
+                }
+            } else if($account['role_name'] == 'admin') {
                 $response = readAccounts();
+                echo json_encode($response);
+                break;
+            } else {
+                header('HTTP/1.1 403 Forbidden');
+                exit();
             }
-            echo json_encode($response);
-            break;
         case 'POST':
             $data = json_decode(file_get_contents('php://input'), true);
             $response = createAccount($data);
             echo json_encode($response);
             break;
         case 'PUT':
-            $data = json_decode(file_get_contents('php://input'), true);
-            $id = $_GET['id'];
-            $id = mysqli_real_escape_string($conn, $id);
-            $response = updateAccount($id, $data);
-            echo json_encode($response);
-            break;
+            if($account['role_name'] == 'admin'){
+                $data = json_decode(file_get_contents('php://input'), true);
+                $id = $_GET['id'];
+                $id = mysqli_real_escape_string($conn, $id);
+                $response = updateAccount($id, $data);
+                echo json_encode($response);
+                break;
+            } else {
+                header('HTTP/1.1 403 Forbidden');
+                exit();
+            }
         case 'DELETE':
-            $id = $_GET['id'];
-            $id = mysqli_real_escape_string($conn, $id);
-            $response = deleteAccount($id);
-            echo json_encode($response);
+            if($account['role_name'] == 'admin'){
+                $id = $_GET['id'];
+                $id = mysqli_real_escape_string($conn, $id);
+                $response = deleteAccount($id);
+                echo json_encode($response);
+            } else {
+                header('HTTP/1.1 403 Forbidden');
+                exit();
+            }
+            
         }
     }
 ?>
--- orders.php
+++ orders.php
@@ -62,7 +62,13 @@ function readOrder($id, $account_id) {
 
     // Check for errors
     if (!$result) {
-        return array('error' => 'Error retrieving orders: ' . mysqli_error($conn));
+        return array('error' => 'Error retrieving order: ' . mysqli_error($conn));
+    }
+
+    if (mysqli_num_rows($result) > 1) {
+        return array('error' => 'Error retrieving order: Multiple orders with same id');
+    } else if (mysqli_num_rows($result) < 1) {
+        return array('error' => 'Order not found.');
     }
 
     // Build the response array
@@ -71,23 +77,29 @@ function readOrder($id, $account_id) {
         $orders[] = $row;
     }
 
-    return $orders;
+    return $orders[0];
 }
 
 // Update an existing order
 function updateOrder($id, $data) {
     global $conn;
 
+    $account_id = mysqli_real_escape_string($conn,$data['added_by']);
+    $response = readOrder($id, $account_id);
+    if(array_key_exists('error', $response)){
+       return array('error' => 'Error updating order: '.$response['error']);
+    }
+
     // Extract the data from the request body
     $battle_id = mysqli_real_escape_string($conn, $data['battle_id']);
     $fight_for = mysqli_real_escape_string($conn, $data['fight_for']);
     $wall = mysqli_real_escape_string($conn, $data['wall']);
     $candies = mysqli_real_escape_string($conn, $data['candies']);
-    $added_by = $data["added_by"];
+    
 
     // Prepare the SQL query
-    $sql = "UPDATE orders SET battle_id='$battle_id', fight_for='$fight_for', wall='$wall', candies='$candies', $added_by = '$added_by' 
-            WHERE id=$id AND added_by = '$added_by'";
+    $sql = "UPDATE orders SET battle_id='$battle_id', fight_for='$fight_for', wall='$wall', candies='$candies', added_by = '$account_id' 
+                WHERE id=$id AND added_by=$account_id";
 
     // Execute the query
     if (mysqli_query($conn, $sql)) {
@@ -101,6 +113,11 @@ function updateOrder($id, $data) {
 function deleteOrder($id, $account_id) {
     global $conn;
 
+    $response = readOrder($id, $account_id);
+    if(array_key_exists('error', $response)){
+       return array('error' => 'Error deleting order: '.$response['error']);
+    }
+
     // Prepare the SQL query
     $sql = "DELETE FROM orders WHERE id=$id AND added_by = $account_id";
 
@@ -113,14 +130,25 @@ function deleteOrder($id, $account_id) {
 }
 
 
-//API key validation
-$apiKey = isset($_GET['api_key']) ? $_GET['api_key'] : '';
-$account = validate_account($apiKey);
 // Handle HTTP requests
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 if(in_array('orders.php', $uri)){
+    //API key validation
+    if(isset($_GET['api_key'])){
+        $apiKey = $_GET['api_key'];
+    } else {
+        header('HTTP/1.1 401 Unauthorized');
+        echo json_encode(array('error' => 'You need to use an api_key param in order to execute requests.'));
+        exit();
+    }
+
+    $account = readAccountByKey($apiKey);
+    if(array_key_exists('error', $account)){
+        header('HTTP/1.1 401 Unauthorized');
+        exit();
+    }
     switch ($method) {
         case 'GET':
             $id = $_GET['id'] ?? null;
@@ -133,12 +161,25 @@ if(in_array('orders.php', $uri)){
             echo json_encode($response);
             break;
         case 'POST':
+            if($account['status'] != 'active'){
+                echo json_encode(array('error' => 'Your account is not active anymore. Please contact the administrator.'));
+                break;
+            }
+            $orders =  readOrders($account['id']);
+            if(count($orders) >= $account['max_orders']){
+                 echo json_encode(array('error' => 'You hit the max limit for active orders.'));
+                 break;
+            } 
             $data = json_decode(file_get_contents('php://input'), true);
             $data['added_by'] = $account['id'];
             $response = createOrder($data);
             echo json_encode($response);
             break;
         case 'PUT':
+            if($account['status'] != 'active'){
+                echo json_encode(array('error' => 'Your account is not active anymore. Please contact the administrator.'));
+                break;
+            }
             $data = json_decode(file_get_contents('php://input'), true);
             $id = $_GET['id'];
             $id = mysqli_real_escape_string($conn, $id);
